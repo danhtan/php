e071e9f82ccd58344b786ba6cce68f93849ecfb9
danhtan <45035071+danhtan@users.noreply.github.com>
2020-10-27 20:47:59 +0700

    Update connect.php

--- sources/php/connect.php
+++ sources/php/connect.php
@@ -1,8 +1,14 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dtb="thitracnghiem";
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "";
+    // $dtb="thitracnghiem";
+
+     $servername = "remotemysql.com:3306";
+     $username = "cKBwKpLNq7";
+     $password = "hErPirQl9s";
+     $dtb="cKBwKpLNq7";
+
     // Create connection
     $conn = mysqli_connect($servername, $username, $password,$dtb);
     if (!$conn) {
